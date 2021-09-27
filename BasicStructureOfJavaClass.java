3df0a5de6ea46a8699525cbda4e205dfecf0cba9
Abhilash <90084823+kothagundua1@users.noreply.github.com>
2021-09-27 12:41:53 +0530

    Add files via upload
    Assignment 1 data

---
+++ BasicStructureOfJavaClass.java
@@ -0,0 +1,16 @@
+package basicsofjava;
+
+public class BasicStructureOfJavaClass {
+	
+	String name="Abhilash";
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing is fun");
+		System.out.println("for everyone");
+		
+	}
+
+}
