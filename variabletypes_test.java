e21f3dfc77c7dff71c4c396fb41c1f43b3274320
Abhilash <90084823+kothagundua1@users.noreply.github.com>
2021-09-27 12:42:33 +0530

    Assignment 1

---
+++ variabletypes_test.java
@@ -0,0 +1,18 @@
+package basicsofjava;
+
+public class variabletypes_test {
+	
+	String name="SDET";
+			
+public static void main(String[] args) {
+		
+	// TODO Auto-generated method stub
+	boolean SDET1 = false;
+	System.out.println("Boolean variable is:"+SDET1);
+	int Age = 10;
+	System.out.println("My Age is:"+Age);
+	double d = 123.4567889;
+	float f = 123.44444f;
+}
+
+}
